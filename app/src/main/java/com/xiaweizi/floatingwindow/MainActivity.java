d33009ed24eece25a5b3d17a81c0dfc7ee4472e5
xiaweizi <1012126908@qq.com>
2018-12-15 10:40:25 +0800

    [dev] 初始化项目

---
+++ app/src/main/java/com/xiaweizi/floatingwindow/MainActivity.java
@@ -0,0 +1,15 @@
+package com.xiaweizi.floatingwindow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int activity_main;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_main);
+    }
+}
